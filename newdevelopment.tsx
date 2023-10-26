f61a4bfe74ffaa40cde35e4eb95fac97bb426341
razuAhmad <abdurrahmanrazu2000@gmail.com>
2023-10-26 11:19:58 +0600

    new branch development

---
+++ newdevelopment.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const newdevelopment = () => {
+    return (
+        <div>
+            <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet, id.</h1>
+        </div>
+    )
+}
+
+export default newdevelopment
